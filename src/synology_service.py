349e09b0391364d52bdfa3381d2bd03ad9cc9011
Dawson Par <dawson@andrews.edu>
2025-05-16 16:58:01 -0500

    Created get_api_info in SynologyService class

--- src/synology_service.py
+++ src/synology_service.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+import requests
+
 from firestore_service import FirestoreService
 from log_config import get_logger
 
@@ -16,23 +18,21 @@ class SynologyService:
             credentials_path=fire_base_credentials_path,
         )
 
-    def upload_file(self, file_path, folder_path):
-        """Uploads a file to Synology NAS."""
-        if not os.path.exists(file_path):
-            logger.error(f"File not found: {file_path}")
-            return None
-
+    def get_api_info(self, base_url):
+        """
+        Returns available APIs. Change query to "all" to see all available APIs.
+        """
         try:
-            # Create the destination folder if it doesn't exist
-            os.makedirs(folder_path, exist_ok=True)
 
-            # Upload the file
-            shutil.copy2(
-                file_path, os.path.join(folder_path, os.path.basename(file_path))
-            )
-
-            logger.info(f"Uploaded {file_path} to {folder_path}")
-            return True
+            info_url = f"{base_url}/query.cgi"
+            params_info = {
+                "api": "SYNO.API.Info",
+                "version": "1",
+                "method": "query",
+                "query": "SYNO.API.Auth,SYNO.FileStation.List, SYNO.FileStation.Upload",
+            }
+            response = requests.get(info_url, params=params_info, verify=False)
+            return response.json()
         except Exception as e:
-            logger.error(f"Error uploading {file_path}: {str(e)}")
-            return False
+            logger.info(f"Error when trying to get API info: {str(e)}")
+            return None
