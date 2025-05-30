b493cf96b8f30464b54d891890ce24f11adf20e1
Dawson Par <dawson@andrews.edu>
2025-05-30 11:42:12 -0500

    changes pageSize on list_files to 100

--- src/google_drive_service.py
+++ src/google_drive_service.py
@@ -109,7 +109,7 @@ class GoogleDriveService:
                 .list(
                     q=f"'{folder_id}' in parents",
                     fields="files(id, name, mimeType)",
-                    pageSize=200,
+                    pageSize=100,
                 )
                 .execute()
             )
